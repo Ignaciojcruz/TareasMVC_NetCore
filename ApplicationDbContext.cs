d9600944eabc3cbd38cfe1e1415b7de782a81971
Ignacio Cruz <ignaciojcruz@hotmail.com>
2023-03-15 20:27:37 -0300

    add entidades entity

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TareasMVC_NetCore.Entidades;
+
+namespace TareasMVC_NetCore
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+
+            //modelBuilder.Entity<Tarea>().Property(t => t.Titulo).IsRequired().HasMaxLength(250);
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }
+        public DbSet<Paso> Pasos { get; set; }
+        public DbSet<ArchivoAdjunto> ArchivosAdjuntos { get; set; }
+
+
+    }
+}
