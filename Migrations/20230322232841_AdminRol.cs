ce631225916356372770d426e97576609a4f070a
Ignacio Cruz <ignaciojcruz@hotmail.com>
2023-03-23 20:24:51 -0300

    add rol migrations

---
+++ Migrations/20230322232841_AdminRol.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC_NetCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS(Select Id from AspNetRoles where Id= '83209fad-65fd-44ae-9a56-a3b24f5a64f7')
+                BEGIN
+                    INSERT AspNetRoles (Id, [Name], [NormalizedName])
+                    VALUES ('83209fad-65fd-44ae-9a56-a3b24f5a64f7','admin','ADMIN')
+                END
+                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE AspNetRoles Where Id = '83209fad-65fd-44ae-9a56-a3b24f5a64f7'");
+
+        }
+    }
+}
