f0989fca6e68e982b537bd2689bc60bf9032b1d6
Ignacio Cruz <ignaciojcruz@hotmail.com>
2023-04-05 18:31:49 -0400

    add modal detalle tareas

--- Controllers/TareasController.cs
+++ Controllers/TareasController.cs
@@ -36,6 +36,19 @@ namespace TareasMVC_NetCore.Controllers
 
         }
 
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Tarea>> Get(int id)
+        {
+            var usuarioId = _servicioUsuarios.ObtenerUsuarioId();
+
+            var tarea = await _context.Tareas.FirstOrDefaultAsync(t => t.Id == id && t.UsuarioCreacionId == usuarioId);
+
+            if(tarea is null) return NotFound();
+
+            return tarea;
+
+        }
+
         [HttpPost]
         public async Task<ActionResult<Tarea>> Post([FromBody] string titulo)
         {
