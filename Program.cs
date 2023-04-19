bbfb0b9626e1c212d9d7d14300c52e6d43d79f78
Ignacio Cruz <ignaciojcruz@hotmail.com>
2023-04-19 14:42:09 -0400

    add implementación Local

--- Program.cs
+++ Program.cs
@@ -61,6 +61,7 @@ builder.Services.AddLocalization( opt =>
 builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddTransient<IAlmacenadorArchivos, AlmacenadorArchivosAzure>();
+builder.Services.AddTransient<IAlmacenadorArchivos, AlmacenadorArchivosLocal>();
 
 var app = builder.Build();
 
